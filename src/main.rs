d0ab96c9caef4763c36f4f077ba1baf5c1778f9e
robert keazor <robertskeazor@gmail.com>
2023-10-21 05:15:21 -0400

    Connect to TCP and listen for a message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (mut socket, _add) = listener.accept().await.unwrap();
+    let mut buffer = [0u8; 1024];
+    let bytes = socket.read(&mut buffer).await.unwrap();
+    socket.write_all(&buffer[..bytes]).await.unwrap();
 }
