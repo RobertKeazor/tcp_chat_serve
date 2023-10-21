53c546eee10fc740f4ed840aeabf8acced6b254f
robert keazor <robertskeazor@gmail.com>
2023-10-21 07:11:42 -0400

    Handle one Client connection

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    print!("We connected");
     let (mut socket, _add) = listener.accept().await.unwrap();
-    let mut buffer = [0u8; 1024];
-    let bytes = socket.read(&mut buffer).await.unwrap();
-    socket.write_all(&buffer[..bytes]).await.unwrap();
+    let(read, mut write) = socket.split();
+    let mut reader = BufReader::new(read);
+    let mut line = String::new();
+    loop {
+        let bytes_read = reader.read_line(&mut  line).await.unwrap();
+        if bytes_read == 0 { break }
+        write.write_all(line.as_bytes()).await.unwrap();
+        line.clear()
+    }
 }
